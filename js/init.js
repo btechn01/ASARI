f74d68619a5f2e347f63adcf714cf3a8adfd3c36
Behnam <btechn01@gmail.com>
2018-01-26 16:45:23 +0330

    961106
    961106

--- js/init.js
+++ js/init.js
@@ -84,22 +84,6 @@
     });
 
 
-    // Github Latest Commit
-    if ($('.github-commit').length) { // Checks if widget div exists (Index only)
-      $.ajax({
-        url: "https://github.com/MahdiMajidzadeh/materialize-rtl/commits/",
-        dataType: "json",
-        success: function (data) {
-          var sha = data.sha,
-              date = jQuery.timeago(data.commit.author.date);
-          if (window_width < 1120) {
-            sha = sha.substring(0,7);
-          }
-          $('.github-commit').find('.date').html(date);
-          $('.github-commit').find('.sha').html(sha).attr('href', data.html_url);
-        }
-      });
-    }
 
     // Toggle Flow Text
     var toggleFlowTextButton = $('#flow-toggle');
